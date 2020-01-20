09dc463a8d9a22242beaeb32dbe90da09f3fe442
natalielyn <nlmaxwell@gmail.com>
2020-01-19 21:07:50 -0800

    added documentation

--- src/routes/v1.js
+++ src/routes/v1.js
@@ -8,6 +8,14 @@ require('../../models/categories.js');
 require('../../models/products.js');
 
 
+/**
+ * sets model to the route that is inputted, or returns a invalid model
+ * @function getModel
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ * @returns model
+ */
 function getModel(req, res, next){
   let model = req.params.model;
   switch(model){
@@ -26,6 +34,9 @@ function getModel(req, res, next){
 
 router.param('model', getModel);
 
+/**
+ * Routes
+ */
 router.get('/api/v1/:model', handleGetAll);
 router.post('/api/v1/:model', handlePost);
 router.get('/api/v1/:model/:id', handleGetOne);
@@ -37,36 +48,76 @@ router.get('/', (req, res) => {
 })
 
 
-// Route Handlers
+/**
+ * handles all requests 
+ * @function handleGetAll
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 function handleGetAll(req, res, next) {
   req.model.get()
     .then(record => res.json(record))
     .catch(next);
-
 }
 
+
+/**
+ * handles and records one request
+ * @function handleGetOne
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 function handleGetOne(req, res, next) {
   let id = req.params.id;
   req.model.get(id)
     .then(record => res.json(record))
     .catch(next);
 }
+
+
+/**
+ * handles route that creates a record
+ * @function handlePost
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 function handlePost(req, res, next) {
   req.model.post(req.body)
     .then(result => res.json(result))
     .catch(next);
 }
 
+
+/**
+ * handles route for updating record
+ * @function handlePut
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 function handlePut(request,response,next) {
   request.model.update(request.params.id, request.body)
     .then( result => response.status(200).json(result) )
     .catch( next );
 }
 
+
+/**
+ * handles route that deletes a record
+ * @function handleGetAll
+ * @param {*} req
+ * @param {*} res
+ * @param {*} next
+ */
 function handleDelete(request,response,next) {
   request.model.delete(request.params.id)
     .then( result => response.status(200).json(result) )
     .catch( next );
 }
-
+/**
+ * @model router
+ */
 module.exports = router;
